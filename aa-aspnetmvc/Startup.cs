d7cd76bf372e5df2676f22dcb0bdfa1e98fdbb27
Angel Anichin <anichin@gmail.com>
2017-04-12 09:55:23 -0600

    first commit

---
+++ aa-aspnetmvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aa_aspnetmvc.Startup))]
+namespace aa_aspnetmvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
